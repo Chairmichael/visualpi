7f53ea962778e03a0c05b99102146539358b0e15
Jefferson Henry <jeffvhenry@gmail.com>
2022-02-21 18:45:16 -0600

    Add a version using matplot

---
+++ matvis.py
@@ -0,0 +1,31 @@
+# matvis.py
+# Uses matplot to draw stuff from the digits of PI
+# this took way to long 😩 (bc first time using matplot)
+
+import matplotlib.pyplot as plt
+import matplotlib.colors as cl
+import numpy as np
+
+
+def main():
+    SIZE = 300
+    plt.style.use('_mpl-gallery')
+    lotsofpi = open('10Kpi.txt').read()
+
+    fig, ax = plt.subplots()
+    x, y, z = [], [], []
+    for i in range(SIZE**2):
+        if lotsofpi[i] == lotsofpi[i+1]:
+            n = int(lotsofpi[i])
+            x += i % SIZE, i % SIZE + 1
+            y += i // SIZE, i // SIZE
+            z += n, n
+
+    ax.scatter(x, y, s=5, c=z, cmap='hsv', alpha=1, edgecolor=None)
+
+    ax.set(xlim=(0, SIZE), ylim=(0, SIZE))
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- vis.py
+++ vis.py
@@ -1,38 +1,40 @@
-#vis.py
+# vis.py
 # draws some dots with pygame
+
 
 import pygame
 from pygame.locals import *
 import colorsys
 
+
 def main():
-	SIZE = 300
-	pygame.init()
-	BLACK = pygame.Color([0,0,0])
+    SIZE = 300
+    pygame.init()
+    BLACK = pygame.Color([0, 0, 0])
 
-	colors = []
-	for x in range(10):
-		r,g,b = [round(i*255) for i in colorsys.hsv_to_rgb(x/10,0.8,0.8)]
-		colors.append(pygame.Color(r,g,b))
-	print(colors)
-	dp = pygame.display.set_mode((SIZE*6,SIZE*6))
-	pygame.display.set_caption("90k digits of pi -- Press any key to exit")
-	lotsofpi = open('10Kpi.txt').read()
-	for i in range(SIZE):
-		for j in range(SIZE):
-			if lotsofpi[j+i*SIZE] == lotsofpi[j+i*SIZE+1]:
-				n = int(lotsofpi[j+i*SIZE])
-				pygame.draw.circle(dp, colors[n], (j*6+2,i*6+2), 2)
-				pygame.draw.circle(dp, colors[n], ((j+1)*6+2,i*6+2), 2)
+    colors = []
+    for x in range(10):
+        r, g, b = [round(i*255) for i in colorsys.hsv_to_rgb(x/10, 0.8, 0.8)]
+        colors.append(pygame.Color(r, g, b))
+    print(colors)
+    dp = pygame.display.set_mode((SIZE*6, SIZE*6))
+    pygame.display.set_caption("90k digits of pi -- Press any key to exit")
+    lotsofpi = open('10Kpi.txt').read()
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if lotsofpi[j+i*SIZE] == lotsofpi[j+i*SIZE+1]:
+                n = int(lotsofpi[j+i*SIZE])
+                pygame.draw.circle(dp, colors[n], (j*6+2, i*6+2), 2)
+                pygame.draw.circle(dp, colors[n], ((j+1)*6+2, i*6+2), 2)
 
-	pygame.display.update()
-	while True:
-		for event in pygame.event.get():              
-			if event.type == QUIT or event.type == KEYUP:
-				pygame.quit()
-				quit()
-		pygame.time.Clock().tick(10)
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or event.type == KEYUP:
+                pygame.quit()
+                quit()
+        pygame.time.Clock().tick(10)
 
 
 if __name__ == '__main__':
-	main()
+    main()
